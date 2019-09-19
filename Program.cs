0f7e9a6374ec4d1182bf563dc7980f9bfb91c318
hchona <hchona1@gmail.com>
2019-09-19 18:19:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ Author: Harrison Chona
+ Date: 09/19/2019
+ Description: With this project. I created a cool little robot with some personality.
+ He asks you a few questions and interacts with your responses.
+*/
+
+using System;
+
+namespace D3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your First and Last name.");
+
+            try
+            {
+                
+                string value = Console.ReadLine();
+
+                Console.WriteLine("Hello " + value + "!");
+            }
+
+            catch
+            {
+                Console.WriteLine("Error... Please enter your First and Last name, separated by a space."); 
+            }
+
+            Console.WriteLine("What is your favorite color?");
+
+            try
+            {
+                string value2 = Console.ReadLine();
+
+                Console.WriteLine("My favorite color is also " + value2 + "!");
+            }
+
+            catch
+            {
+                Console.WriteLine("Error... Please enter your favorite color!");
+            }
+
+            Console.WriteLine("... Do you want to be my friend? Type 0 for yes, 1 for no.");
+
+            try
+            {
+
+                string input = Console.ReadLine();
+
+                int value3 = int.Parse(input);
+
+                if (value3 == 0)
+                {
+                    Console.WriteLine("Well, you just made my day!");
+                }
+
+                else if (value3 == 1)
+                {
+                    Console.WriteLine("Well I was just kidding... And will use your identity to conquer the world!!!");
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Error... Enter the value 0 or 1");
+            }
+
+        }
+    }
+}
